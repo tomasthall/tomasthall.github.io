3af4525b4e822d07458ec421319c082d12e8580d
tomasthall <gebol1@onet.eu>
2019-02-10 22:54:10 +0100

    feat(app): change the way of displaying app

--- app.js
+++ app.js
@@ -1,79 +1,43 @@
+let sections = Array.prototype.slice.call(document.getElementsByClassName('section'));
+let currentPage = 1;
+sections.map(section => `section${currentPage}` !== section.id ? section.style.opacity = '0' : section.style.opacity = '1');
+
+sections.map((section, index) => {
+  section.style.zIndex = sections.length - index;
+});
 
 
+// TODO: Implement scroll
+// let isScrolling;
 //
-//
-// var animals = [
-//   { name: 'Fluffykins', species: 'rabbit' },
-//   { name: 'Caro', species: 'dog'},
-//   { name: 'Hamilton', species: 'dog'},
-//   { name: 'Harold', species: 'fish'},
-//   { name: 'Ursula', species: 'cat'},
-//   { name: 'Jimmy', species: 'fish'}
-// ]
-//
-// var isFish = function(animal){
-//   return animal.species === 'fish';
+// window.onscroll = function (e) {
+//   console.log(this.oldScroll > this.scrollY);
+//   this.oldScroll = this.scrollY;
 // }
-//
-// var isNotFish = function(animal){
-//   return animal.species !== 'fish';
-// }
-//
-// var fish = animals.filter(isFish);
-// var otherAnimals = animals.filter(isNotFish);
-//
-// // console.log(otherAnimals);
-// // var names = animals.map(function(animal){
-// //   return animal.name;
-// // })
-//
-// var names = animals.map((animal) => animal.name)
-//
-// // console.log(names);
-//
-// var orders = [
-//   { amount: 250 },
-//   { amount: 400 },
-//   { amount: 100 },
-//   { amount: 325 }
-// ]
-//
-// // var totalAmount = orders.reduce(function(sum, order){
-// //   return sum + order.amount;
-// // }, 0);
-//
-// var totalAmount = orders.reduce((sum,order) => sum + order.amount, 0)
-//
-// console.log(totalAmount);
 
-// var dataTxt = "mark johansson  waffle iron 80  2\nmark johannson  blender 200 1\nmark johansson knife 10 4\nNikita Smith  waffle iron 80  1\nNikita Smith  knife 10 2\nNikita Smith  pot 20  3";
-//
-// var output = dataTxt
-//     .trim()
-//     .split('\n')
-//     .map(line => line.split('\t'))
-//     .reduce((customers, line)=> {
-//       customers[line[0]] = customers[line[0]] || []
-//     })
-//
-// console.log('output', output);
+window.addEventListener('keydown', (e)=> {
+  if(currentPage >= 1 && currentPage <= sections.length) {
+    if(e.keyCode == 37) {
+        previousSection();
+    }
+    if(e.keyCode == 39) {
+        nextSection();
+    }
+  };
+})
 
-// let dragon = (name, size, element) =>
-//   name + ' is a ' + size + ' dragon that breathes ' + element + '!';
-//
-// console.log(dragon('deathstar', 'enormous', 'fog'));
+function nextSection() {
+  if(currentPage < sections.length) {
+      currentPage++;
+      sections.map(section => `section${currentPage}` !== section.id ? section.style.opacity = '0' : section.style.opacity = '1');
+  }
+}
 
-let dragons = [
-  { name: 'memik', element: 'lighting' },
-  { name: 'kulecnik', element: 'lighting' },
-  { name: 'dudu', element: 'fire' },
-  { name: 'xdman', element: 'timewarp' }
-]
+// TODO: check why that stupid nav sucks balls
 
-let hasElement =
-  (element, obj) => obj.element === element
-
-let lightingDragons =
-  dragons.filter(dragon => hasElement('lighting', dragon))
-
-console.log(lightingDragons);
+function previousSection() {
+  if(currentPage > 1) {
+      currentPage--;
+      sections.map(section => `section${currentPage}` !== section.id ? section.style.opacity = '0' : section.style.opacity = '1');
+  }
+}
